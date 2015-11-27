62755bc58ab8df1536a3e816a541687fb66224cb
Tracey Eubanks <tracey@bypassmobile.com>
2015-11-27 10:09:18 -0600

    base stuff working

---
+++ app/js/app.js
@@ -0,0 +1,3 @@
+angular.module("ExchangeUI", [
+  "ExchangeUI.controllers"
+]);
---
+++ app/js/controllers.js
@@ -0,0 +1,4 @@
+angular.module('ExchangeUI.controllers', [])
+.controller('someController', ['$scope', function($scope) {
+  $scope.live_text = "I'm text!!";
+}]);
